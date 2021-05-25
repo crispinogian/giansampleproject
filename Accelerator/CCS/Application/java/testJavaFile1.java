6bbba19b54feb2d3859af2eeb07358fe45d79bf8
gcrispino <gcrispino@originutility.com>
2021-05-25 21:32:51 +0800

    test files

---
+++ Accelerator/CCS/Application/java/testJavaFile1.java
@@ -0,0 +1,29 @@
+/*                                                              
+ *******************************************************************************
+ *                                                                
+ * PROGRAM DESCRIPTION:                                           
+ *                                                                
+ * Test Java File 1
+ * 
+ * This file is a test java file for searching code snippets
+ *
+ * CODE REUSE TAGS
+ *
+ * #ITRON
+ * #smartMeter
+ * #blacklotus
+ * #BlueEyesWhiteDragon
+ *
+ *******************************************************************************
+ *                                                                    
+ *                                       
+ *******************************************************************************
+ */
+package com.com.com
+
+public class testJavaFile1 {
+
+	public static void main(){
+		
+	}
+}
---
+++ Accelerator/CCS/Application/service/testJavaFile2.java
@@ -0,0 +1,27 @@
+/*                                                              
+ *******************************************************************************
+ *                                                                
+ * PROGRAM DESCRIPTION:                                           
+ *                                                                
+ * Test Java File 2
+ * 
+ * This file is a test java file for searching code snippets
+ *
+ * CODE REUSE TAGS
+ *
+ * #Landis
+ * #SIEMENS
+ *
+ *******************************************************************************
+ *                                                                    
+ *                                       
+ *******************************************************************************
+ */
+package com.com.com
+
+public class testJavaFile2 {
+
+	public static void main(){
+		
+	}
+}
